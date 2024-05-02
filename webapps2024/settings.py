15d510960432a397d32bc2180982716211087bb5
fakitayran <aridurufakitayran@gmail.com>
2024-05-02 14:05:37 +0100

    db password added

--- webapps2024/settings.py
+++ webapps2024/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER": "postgres",
-        "PASSWORD": "",
+        "PASSWORD": "23323847lol",
         "HOST": "localhost",
         "PORT": "5432",
     }
